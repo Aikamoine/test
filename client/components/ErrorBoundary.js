234f0f04a40a028ce4230add44f8c716ff50dc75
Aikamoine <atte.laakso@helsinki.fi>
2022-07-10 09:45:06 +0300

    bootstrap changes

---
+++ client/components/ErrorBoundary.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+
+class ErrorBoundary extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      hasError: false,
+    }
+  }
+
+  static getDerivedStateFromError() {
+    return { hasError: true }
+  }
+
+  componentDidCatch() {
+    this.setState({ hasError: true })
+  }
+
+  render() {
+    const { hasError } = this.state
+    const { children } = this.props
+    console.log('children', children)
+    if (!hasError) {
+      return children
+    }
+    return <p> An error occurred! </p>
+  }
+}
+
+export default ErrorBoundary
